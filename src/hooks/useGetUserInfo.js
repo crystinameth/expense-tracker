4162cd165ebdde62d5d4d04fbd18aa0af5ee220d
shivimishra <shivimishra130103@gmail.com>
2023-12-16 03:40:20 +0530

    firebase deploy

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,6 +1,5 @@
 export const useGetUserInfo = () => {
-    const { name, profilePicture, userID, isAuth } = JSON.parse(
-        localStorage.getItem("auth")
-    );
-    return { name, profilePicture, userID, isAuth };
+  const { name, profilePicture, userID, isAuth } =
+    JSON.parse(localStorage.getItem("auth")) || {}; // empty object needed because if there is no auth in local storage, it will return null and null.name will throw an error , deployment error
+  return { name, profilePicture, userID, isAuth };
 };
