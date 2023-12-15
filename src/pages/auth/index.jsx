27462ba8a220e7df0fe34933fce3e1c62fef3b70
shivimishra <shivimishra130103@gmail.com>
2023-12-15 16:24:41 +0530

    first commit

---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,30 @@
+import { auth, provider } from "../../config/firebase-config"
+import { signInWithPopup } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
+
+export const Auth = () => {
+
+    const navigate = useNavigate();
+
+    const signInWithGoogle = async () => {
+       const results = await signInWithPopup(auth, provider);                    //this is an async function, returns a promise
+    //  console.log(results);                                                   But we need to store the user data in the local storage, we cannot store objects in local storage, so we need to convert it into a string
+       const authInfo = {
+            userID: results.user.uid,
+            name: results.user.displayName,
+            profilePicture: results.user.photoURL,
+            isAuth: true,
+       };
+        localStorage.setItem("auth", JSON.stringify(authInfo));
+        navigate("/expense-tracker")                  //JSON.stringify() converts the object into a string
+    }
+
+    return (
+        <div className="login-page">
+            <p>Sign In With Google to Continue</p>
+            <button className="login-with-google-btn" onClick={signInWithGoogle}>
+                Sign In
+            </button>
+        </div>
+    )
+}
---
+++ src/pages/expense-tracker/index.jsx
@@ -0,0 +1,7 @@
+export const ExpenseTracker = () => {
+    return (
+        <div>
+            <h1>Expense Tracker</h1>
+        </div>
+    )
+}
